2b58d5f1d9360cef17217dfd81f066905a273985
IamShahzaibAli-SA <243137271+IamShahzaibAli-SA@users.noreply.github.com>
2025-11-23 14:08:56 +0500

    API Working:

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState(null)
+
+  const fetchAPI = async () => {
+    const url = "https://api.nekosia.cat/api/v1/images/catgirl";
+    const response = await fetch(url);
+    const result = await response.json();
+    const finalurl = result.image.original.url;
+    setData(finalurl);
+
+    console.log(result.image.original.url);
+  }
+
+  useEffect(()=>{
+    fetchAPI();
+  }, []);
 
   return (
     <>
-      
+      <div>
+        <img src={data} alt="" />
+      </div>
     </>
   )
 }
